6a09e854cdc84c571811e822dd9b5b785d71ea61
Bora Aydın <boraaydins@gmail.com>
2019-10-08 18:23:13 +0300

    Add files via upload

---
+++ IP Week1/Hafta1.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[25]:
+
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+os.getcwd()
+os.listdir()
+
+img_1 = plt.imread('C:\\Users\\BorA\\Downloads\\Resimler\\bsen.jpg')
+
+img_1.ndim
+img_1.shape
+m,n,p = img_1.shape
+m,n,p
+
+plt.imshow(img_1)
+plt.show()
+
+img_1[100,100,:]
+
+new_image=np.zeros((m,n),dtype=float)
+
+for i in range(m):
+    for j in range(n):
+        s = (img_1[i,j,0] + img_1[i,j,1] + img_1[i,j,2])/3
+        new_image [i,j] = s
+
+plt.imshow(new_image,cmap='gray') 
+plt.show()          
+
+plt.imsave('bsen2.jpg',new_image,cmap='gray')
+
+transpoze_image=np.zeros((n,m),dtype=float)
+
+for i in range(m):
+    for j in range(n):
+        s = (img_1[i,j,0] + img_1[i,j,1] + img_1[i,j,2])/3
+        transpoze_image [j,i] = s
+        
+plt.imshow(transpoze_image,cmap='gray') 
+plt.show() 
+plt.imsave('bsen90.jpg',transpoze_image,cmap='gray')
+
+
